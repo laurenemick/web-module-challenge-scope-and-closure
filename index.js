2977ec8a7f0df71ac26250206ddf9ac29ffc9d3a
Lauren Emick <laurenemick6@gmail.com>
2020-05-18 12:41:27 -0700

    completed

--- index.js
+++ index.js
@@ -64,15 +64,15 @@ function counter2() {
 Write a function called `inning` that generates a random number of points that a team scored in an inning. 
 This should be a whole number between 0 and 2. */
 
-function inning(team){
-  let points = Math.floor(Math.random() * Math.floor(2));
-  return `Your ${team} team scored ${points} points`;
+function inning(){
+  return Math.floor(Math.random() * Math.floor(3));
 }
-console.log(inning(baseball);
+/*console.log(inning());*/
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) 
+and a number of innings and and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -84,11 +84,17 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(cb, num){
+  var home = 0;
+  var away = 0;
 
-  /*Code Here*/
-
+  for (let i = 0; i < num; i++){
+    home = home + inning();
+    away = away + inning();
+  }
+  return {Home: home, Away: away};
 }
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -111,8 +117,27 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function scoreboard(cb, num) {
+  var home = 0;
+  var away = 0;
+  var inningNum = 0;
 
+  for (let i = 0; i < num; i++){
+    home = home + inning();
+    away = away + inning();
+    inningNum = inningNum + 1;
+
+    if (inningNum === 1) {
+      console.log(`${inningNum}st inning: ${home} - ${away}`);
+    } else if (inningNum === 2) {
+      console.log(`${inningNum}nd inning: ${home} - ${away}`);
+    } else if (inningNum === 3) {
+      console.log(`${inningNum}rd inning: ${home} - ${away}`);
+    } else {
+      console.log(`${inningNum}th inning: ${home} - ${away}`);
+    }
+  }
+  return `Final Score: ${home} - ${away}`;
+}
+console.log(scoreboard(inning, 9));
 
